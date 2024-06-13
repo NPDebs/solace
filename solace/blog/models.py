de9d00596e569ccb65d5ed989f0109e2f8974f4e
NPDebs <deborahudoh02@gmail.com>
2024-06-13 22:10:41 +0100

    Create model for comments.

--- solace/blog/models.py
+++ solace/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# Create model for post category
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
@@ -8,5 +9,11 @@ class Post(models.Model):
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    categories = models.ManyToManyField("Category", related_name="posts") # Assign many categories to many posts
+    categories = models.ManyToManyField("Category", related_name="posts") # Assign many categories to many posts (Many-to-many relationship)
 
+# Create model for comments
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE) # Assign multiple comments to a single post (Many-to-one relationship). Delete comments if post is deleted.
