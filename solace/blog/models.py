61f5741b80f632d3908493d590727bcfe171f4b9
NPDebs <deborahudoh02@gmail.com>
2024-06-14 23:59:32 +0100

    Use correct string representation.

--- solace/blog/models.py
+++ solace/blog/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create model for post category
 class Category(models.Model):
     name = models.CharField(max_length=30)
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name   
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -11,9 +16,16 @@ class Post(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField("Category", related_name="posts") # Assign many categories to many posts (Many-to-many relationship)
 
+    def __str__(self):
+        return self.title
+    
 # Create model for comments
 class Comment(models.Model):
     author = models.CharField(max_length=60)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey("Post", on_delete=models.CASCADE) # Assign multiple comments to a single post (Many-to-one relationship). Delete comments if post is deleted.
+
+
+    def __str__(self):
+        return f"{self.author} on '{self.post}'"
