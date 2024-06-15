3a19ac15d5893603a946b0a88c36f31e26965a10
NPDebs <deborahudoh02@gmail.com>
2024-06-15 01:01:21 +0100

    Create views to render i) all posts, ii) posts by category and iii) each post in detail.

--- solace/blog/views.py
+++ solace/blog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from blog.models import Post, Comment
 
-# Create your views here.
+# Create view to display a list of all posts
+def blog_index(request):
+    # Get all posts in the database; arrange in reverse order of date created
+    posts = Post.objects.all().order_by("-created_on")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog/index.html", context)
+
+# Create view to display posts under a specific category
+def blog_category(request, category):
+    # Get all posts; filter using category passed as argument; sort to display most recent first
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by("-created_on")
+    context = {
+        "category": category,
+        "posts": posts,
+    }
+    return render(request, "blog/category.html", context)
+
+# Create view to display each post in detail (i.e full post, comment)
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "blog/detail.html", context)
